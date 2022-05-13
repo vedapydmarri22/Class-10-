87a0448f663f3b810976721b3bfcc1ca001b17c7
vedapydmarri22 <105470226+vedapydmarri22@users.noreply.github.com>
2022-05-12 22:02:04 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var box 
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (303, 206, 50, 50)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites ()
+box.shapeColor = "red"
+}
